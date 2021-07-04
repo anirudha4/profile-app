fadaf4bf9455007479a88a0d8530927cc4001b8c
unknown <anirudhag1999@gmail.com>
2021-07-04 13:35:33 +0530

    fixed masonry

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from './navbar.module.css'
 import { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import FeatherIcon from 'feather-icons-react'
+import logo from '../assets/logo.svg'
 export default function Navbar() {
     const {user, login, logout, userMeta} = useContext(AuthContext)
     return (
         <nav className={styles.navbar}>
-            <div className={styles.right}>
+            <div className={styles.left}>
                 <Link href="/">
-                    <a className={styles.logo}>LOGO</a>
+                    <Image className={styles.logo} src={logo} width={80} height={40} />
                 </Link>
             </div>
             <div className={styles.right}>
@@ -27,7 +29,7 @@ export default function Navbar() {
                         <Link href="/profile">
                             <a className={styles.profile}>A</a>
                         </Link>
-                        <button className={styles.logout} onClick={logout}>Logout <FeatherIcon icon='log-out' size={18} color='red' /></button>
+                        <button className={styles.logout} onClick={logout}><span>Logout</span> <FeatherIcon icon='log-out' size={18} color='red' /></button>
                     </>
                 )}
             </div>
